4a7cd507238c11c0144f0725abdbeb76e032100e
Plus10Victory <plustenvictory@gmail.com>
2024-12-30 15:42:51 -0800

    added src/ textnode.py & main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from textnode import *
+
+def main():
+    sample_text = "This is a text node"
+    sample_type = TextType.BOLD
+    sample_url = "https://www.boot.dev"
+
+    sample_node = TextNode(sample_text, sample_type, sample_url)
+    
+    print(sample_node.__repr__())
+
+
+main()
