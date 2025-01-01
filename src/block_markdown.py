c9298a29e590d1b68f65089606368f34ea6e7933
Plus10Victory <plustenvictory@gmail.com>
2025-01-01 10:38:42 -0800

    creating framework for  Block to HTML functions

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,6 @@
 import re
 
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -33,3 +34,29 @@ def block_to_block_type(block):
 
 
 def markdown_to_html_node(markdown):
+    #split markdown into blocks
+    blocks = markdown_to_blocks(markdown)
+    #loop over each block
+    for block in blocks:
+        # determine the type of block
+        # create new HTMLNode with the proper data
+        # assign the proper child HTMLNode (text_to_children)
+    # make all block nodes children of <div> HTMLNode
+
+
+
+def text_to_children(text):
+    # works for all block types
+    # take string of text
+    # use previously created functions
+    # TextNode -> HTMLNode
+    # return list of HTMLNodes
+
+
+# create functions block -> HTMLNode for each type
+# Quote <blockquote></blockquote>
+# unordered_list <ul><li></li></ul>
+# ordered_list <ol><li></li></ol>
+# code <pre><code></code></pre>
+# headings <h{i}></h{i}> i = len(string(#))
+# paragraph <p></p>
